118fce3cd6cea47388e7f651edcca95e417868c5
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-03-12 03:32:05 +0600

    modal has been added here and dynamic functionality

--- script/app.js
+++ script/app.js
@@ -28,7 +28,11 @@ const loadVideos = async () => {
     try {
         const response = await fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
         const data = await response.json()
-        displayVideos(data.videos)
+        {
+            removeActiveClass()
+            document.getElementById('btn-all').classList.add('active')
+            displayVideos(data.videos)
+        }
     }
     catch (error) {
         console.log(error)
@@ -66,6 +70,7 @@ const displayVideos = (videos) => {
                 <img class="w-4" src="assets/verified.png" alt="">
             </div>
             <small class="pl-14 text-sm text-[#171717B3]">${video.others.views} Views</small>
+            <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block mt-3">Show Details</button>
         </div>
         
         `
@@ -76,9 +81,46 @@ const displayVideos = (videos) => {
 const loadCategoryVideo = (id) => {
     const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`
     fetch(url).then(res => res.json()).then(data => {
-        const clickedButton= document.getElementById(`btn-${id}`)
+        removeActiveClass()
+        const clickedButton = document.getElementById(`btn-${id}`)
         clickedButton.classList.add('active')
         displayVideos(data.category)
         console.log(clickedButton);
     })
+}
+
+const removeActiveClass = () => {
+    const activeButtons = document.getElementsByClassName('active')
+    for (let btn of activeButtons) btn.classList.remove('active')
+}
+
+const loadVideoDetails = (videoId) => {
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+    fetch(url)
+        .then(res => res.json())
+        .then((data) => {
+            displayVideoDetails(data.video)
+        })
+
+}
+const displayVideoDetails = (video) => {
+    document.getElementById('video_details').showModal()
+    const detailsContainer = document.getElementById('details-container')
+    detailsContainer.innerHTML = `
+        <div>
+           <div class="relative">
+            <img class="rounded-md w-full h-[200px] object-cover" src="${video.thumbnail}" alt="">
+            <span class="absolute bottom-3 right-4 bg-black rounded-sm text-white text-sm px-1">${video.others['posted_date']}</span>
+           </div>
+            <div class="flex items-center gap-4 mt-3">
+                <img class="w-10 h-10 rounded-full" src="${video.authors[0]['profile_picture']}" alt="">
+                <h3 class="font-bold">Shape of You</h3>
+            </div>
+            <div class="flex items-center gap-2 mx-auto pl-14">
+                
+            <small class="text-sm text-[#171717B3]">${video.description} Views</small>
+            </div>
+            
+        </div>
+        `
 }
